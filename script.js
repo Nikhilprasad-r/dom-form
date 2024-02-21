197d6dee0b6e5f2b28f5d4c9934e0eb2a5eae57d
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-02-21 10:42:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 const formContainer = document.createElement("div");
 document.body.append(formContainer);
+
 const form = document.createElement("form");
 form.id = "myForm";
 
-const labels = ["First Name:", "Middle Name:", "Last Name:", "Email:"];
-const ids = ["firstName", "middleName", "lastName", "email"];
-const types = ["text", "text", "text", "email"];
+const labels = [
+  "First Name:",
+  "Middle Name:",
+  "Last Name:",
+  "Email:",
+  "pincode",
+  "city",
+];
+const ids = ["firstName", "middleName", "lastName", "email", "pincode", "city"];
+const types = ["text", "text", "text", "email", "text", "text"];
 
 for (let i = 0; i < labels.length; i++) {
+  elementCreate(ids[i], labels[i], types[i]);
+}
+
+function elementCreate(id, labels, type) {
   const label = document.createElement("label");
-  label.htmlFor = ids[i];
-  label.textContent = labels[i];
+
+  label.htmlFor = id;
+
+  label.textContent = labels;
+
   form.appendChild(label);
-
-  const input = document.createElement("input");
-  input.type = types[i];
-  input.id = ids[i];
-  input.name = ids[i];
-  form.appendChild(input);
-
   form.appendChild(document.createElement("br"));
+  const input = document.createElement("input");
+  input.type = type;
+  input.id = id;
+  input.name = id;
+  form.appendChild(input);
   form.appendChild(document.createElement("br"));
 }
+formContainer.appendChild(form);
 
 const submitBtn = document.createElement("input");
 submitBtn.type = "submit";
 submitBtn.value = "Submit";
 form.appendChild(submitBtn);
-
-formContainer.appendChild(form);
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -37,9 +49,12 @@ form.addEventListener("submit", function (event) {
   const middleName = document.getElementById("middleName").value;
   const lastName = document.getElementById("lastName").value;
   const email = document.getElementById("email").value;
-
+  const pincode = document.getElementById("pincode").value;
+  const city = document.getElementById("city").value;
   console.log("First Name:", firstName);
   console.log("Middle Name:", middleName);
   console.log("Last Name:", lastName);
   console.log("Email:", email);
+  console.log("Pincode", pincode);
+  console.log("City", city);
 });
